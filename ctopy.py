e91c69dff6ed9bfcddc45b34e56b41ff54d9c723
Stefan Delibas <delibas.stefan@gmail.com>
2020-05-24 16:32:26 +0300

    added cleanup

--- ctopy.py
+++ ctopy.py
@@ -2,6 +2,16 @@ from setuptools import setup
 import argparse
 import subprocess
 import sys
+import os
+import shutil
+import glob
+
+debug = True
+
+def clean():
+    shutil.rmtree("Release")
+    shutil.rmtree("__pycache__")
+    os.remove("cffi_module.c")
 
 def get_c(headers, sources):
     cmd = [sys.executable , 'build_cffi.py', "--inc"," ".join(headers), "--src", " ".join(sources)]
@@ -14,6 +24,10 @@ def get_c(headers, sources):
     #import only after the build is done
     import build_py_wrapper 
     mw = build_py_wrapper.get_pywrap()
+
+    if not debug:
+        clean()
+
     return mw
 
 if __name__ == "__main__":
@@ -22,6 +36,10 @@ if __name__ == "__main__":
                         help='path to H header files.')
     parser.add_argument('--src', type=str, nargs='+', required=True,
                         help='path to C source files.')
+    parser.add_argument('-d', default=False, action="store_true",
+                        help='Debug flag. keep all intermediary files')
     args = parser.parse_args()
 
+    if args.d:
+        debug = args.d
     get_c(args.inc, args.src)
